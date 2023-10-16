d56e75a6126505a168300210a441b2af70b552bb
Zmoore-MCCC <143642207+Zmoore-MCCC@users.noreply.github.com>
2023-10-16 16:49:23 -0400

    added saving high score
    added saving high score

--- BasicMovementJump/Assets/Scripts/GameManager.cs
+++ BasicMovementJump/Assets/Scripts/GameManager.cs
@@ -5,9 +5,13 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     private bool gameOver;
+    public GameObject player;
+    private PlayerScore playerScore;
 
     private void Start()
     {
+        playerScore = player.GetComponent<PlayerScore>();
+        Debug.Log("High Score: " + SaveData.loadScore());
         setGameOver(false);
     }
 
@@ -26,6 +30,7 @@ public class GameManager : MonoBehaviour
     {
         if(gameOver)
         {
+            SaveData.saveScore(playerScore.getScore());
             Time.timeScale = 0f;
         }
         else
